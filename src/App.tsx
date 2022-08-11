56a25cb816ffe4504c39e2851de90d2707ad13fa
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-08-10 22:38:32 -0300

    feat: added debug observer recoil

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Formulario from './components/Formulario';
 import Calendario from './components/Calendario';
 import ListaDeEventos from './components/ListaDeEventos';
 import { RecoilRoot } from 'recoil';
+import DebugObserver from './components/DebugObserver';
 
 function App() {
   const [filtro, setFiltro] = useState<Date | null>()
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <RecoilRoot>
+      <DebugObserver />
       <Suspense fallback='Está carregando.'>
         <div className={style.App}>
           <div className={style.Coluna}>
